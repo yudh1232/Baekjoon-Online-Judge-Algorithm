6b58bc270b0da90693952925c2f4576708ecae37
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-08-10 10:15:07 +0900

    Update 2578 빙고.java

--- 2578 빙고.java
+++ 2578 빙고.java
@@ -6,6 +6,7 @@ public class Main {
 	static int[][] grid;
 	static boolean[][] selected;
 	
+	// 불린 수를 체크함
 	private static void checkNum(int num) {
 		for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++) {
@@ -17,9 +18,12 @@ public class Main {
 		}
 	}
 	
+	// 빙고가 생겼는지 체크
 	private static int checkBingo() {
 		boolean bingoFlag;
 		int bingoCnt = 0;
+		
+		// 가로줄 체크
 		for (int i = 0; i < 5; i++) {
 			bingoFlag = true;
 			for (int j = 0; j < 5; j++) {
@@ -31,6 +35,7 @@ public class Main {
 			if (bingoFlag == true) bingoCnt += 1;
 		}
 		
+		// 세로줄 체크
 		for (int j = 0; j < 5; j++) {
 			bingoFlag = true;
 			for (int i = 0; i < 5; i++) {
@@ -42,12 +47,14 @@ public class Main {
 			if (bingoFlag == true) bingoCnt += 1;
 		}
 		
+		// 대각선 체크
 		bingoFlag = true;
 		for (int i = 0; i < 5; i++) {
 			if (selected[i][i] == false) bingoFlag = false;
 		}
 		if (bingoFlag == true) bingoCnt += 1;
 		
+		// 대각선 체크
 		bingoFlag = true;
 		for (int i = 0; i < 5; i++) {
 			if (selected[i][4 - i] == false) bingoFlag = false;
@@ -60,9 +67,10 @@ public class Main {
 	public static void main(String[] args) throws Exception {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		
-		grid = new int[5][5];
-		selected = new boolean[5][5];
+		grid = new int[5][5]; // 빙고판 2차원 배열
+		selected = new boolean[5][5]; // 불린 수를 체크하는 2차원 배열
 		
+		// 빙고판에 숫자를 입력받음
 		for (int i = 0; i < 5; i++) {
 			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
 			for (int j = 0; j < 5; j++) {
@@ -74,9 +82,11 @@ public class Main {
 		for (int i = 0; i < 5; i++) {
 			StringTokenizer st = new StringTokenizer(in.readLine(), " ");
 			for (int j = 0; j < 5; j++) {
+				// 불린 수를 체크함
 				checkNum(Integer.parseInt(st.nextToken()));
+				// 빙고가 3개이상 나왔다면
 				if (checkBingo() >= 3) {
-					System.out.println(5 * i + j + 1);
+					System.out.println(5 * i + j + 1); // 결과 출력
 					break loop;
 				}
 			}
