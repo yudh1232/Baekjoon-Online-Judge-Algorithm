361b4d2ce0140928e2e3c2d99f4035e43e2ae033
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-09-08 02:20:13 +0900

    Update 18111 마인크래프트.py

--- 18111 마인크래프트.py
+++ 18111 마인크래프트.py
@@ -43,3 +43,55 @@ for h in range(min_h, max_h + 1):
 
 # 결과 출력
 print(answer_t, answer_h)
+
+# 풀이 2
+"""
+import sys
+input = sys.stdin.readline
+
+# 땅 높이 정보를 담을 리스트
+land = [0] * 257
+
+# n, m, b를 입력받음
+n, m, b = map(int, input().split())
+
+max_h = 0
+min_h = 256
+
+# 땅 높이 정보를 입력받음
+for _ in range(n):
+    data = list(map(int, input().split()))
+    for d in data:
+        max_h = max(max_h, d)
+        min_h = min(min_h, d)
+        land[d] += 1
+
+# 답 시간과 높이 초기화
+answer_t = int(1e9)
+answer_h = min_h
+
+# h: 고르게 했을 때의 높이
+for h in range(min_h, max_h + 1):
+    t = 0
+    temp_b = b
+
+    for i in range(min_h, max_h + 1):
+        # 블록을 놓는 경우
+        if h > i:
+            t += (h - i) * land[i]
+            temp_b -= (h - i) * land[i]
+        # 블록을 제거하는 경우
+        else:
+            t += 2 * (i - h) * land[i]
+            temp_b += (i - h) * land[i]
+    
+    # 블록을 채워넣을 수 없거나, 이전에 비해 시간이 많이 걸린 경우 break
+    if temp_b < 0 or t > answer_t:
+        break
+    
+    answer_t = t
+    answer_h = h
+
+# 결과 출력
+print(answer_t, answer_h)
+"""
