131aa3e247b3009c639f60e684a0a48c49b81fc3
yudh1232 <86055115+yudh1232@users.noreply.github.com>
2021-10-10 18:55:27 +0900

    Update 14500 테트로미노.py

--- 14500 테트로미노.py
+++ 14500 테트로미노.py
@@ -57,3 +57,71 @@ for i in range(n):
 
 # 결과 출력
 print(answer)
+
+# 풀이 2
+"""
+import sys
+input = sys.stdin.readline
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+# dfs방식으로 테트로미노를 놓아봄
+def dfs(x, y, count, total):
+    global answer
+
+    # 프루닝
+    if total + (4 - count) * max_val <= answer:
+        return
+
+    # 테트로미노를 놓았으면 결과를 최대로 갱신
+    if count == 4:
+        answer = max(answer, total)
+        return
+    
+    # 상하좌우로 다음칸을 탐색
+    for d in range(4):
+        nx = x + dx[d]
+        ny = y + dy[d]
+        if nx < 0 or nx >= n or ny < 0 or ny >= m:
+            continue
+        if visited[nx][ny]:
+            continue
+
+        # ㅗ, ㅓ, ㅜ, ㅏ 모양 처리
+        if count == 2:
+            visited[nx][ny] = True
+            dfs(x, y, count + 1, total + paper[nx][ny])
+            visited[nx][ny] = False
+
+        visited[nx][ny] = True
+        dfs(nx, ny, count + 1, total + paper[nx][ny])
+        visited[nx][ny] = False
+
+# n, m을 입력받음
+n, m = map(int, input().split())
+
+# 종이 리스트 생성
+paper = []
+# 종이 정보를 입력받음
+for _ in range(n):
+    paper.append(list(map(int, input().split())))
+# 종이에 있는 최대값
+max_val = max(map(max, paper))
+
+# dfs를 위한 visited 리스트 생성
+visited = [[False] * m for _ in range(n)]
+
+# 결과값 변수
+answer = 0
+
+# 각 칸에 테트로미노를 놓아봄
+for i in range(n):
+    for j in range(m):
+        visited[i][j] = True
+        dfs(i, j, 1, paper[i][j])
+        visited[i][j] = False
+
+# 결과 출력
+print(answer)
+"""
